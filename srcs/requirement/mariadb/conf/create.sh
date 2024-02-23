d99c176c57dece9f2ff1837432be3f86c1c3f55a
Leo <leonardlevasseur@yahoo.com>
2024-02-23 19:30:19 +0100

    ¯\_(ツ)_/¯

--- srcs/requirement/mariadb/conf/create.sh
+++ srcs/requirement/mariadb/conf/create.sh
@@ -5,10 +5,11 @@ CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'localhost' IDENTIFIED BY '${SQL_USER_
 GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_USER_PW}'; \
 ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PW}'; \
 FLUSH PRIVILEGES;"
-#printf "Lunch mysqladmin command\n"
-echo "ls " `ls -R /var/run/mysqld`
-echo "ls " `ls -R /run/mysqld`
-mysqladmin -p${SQL_ROOT_PW} shutdown
+printf "Lunch mysqladmin command\n"
+chmod 755 -R /var/run/mysqld
+chown root:root /var/run/mysqld	#/var/run/mysqld.sock is deleted without this line ¯\_(ツ)_/¯
+mysqladmin -u root -p${SQL_ROOT_PW} shutdown
+echo 'ls' `ls -R /var/run/mysqld`
 #printf "Lunch mysqld_safe\n"
 #exec mysqld_safe
 
