233fb62ecedb7b73894f27bda2b8d3fb3ae3e368
llevasse <leonardlevasseur@yahoo.com>
2024-02-27 10:58:49 -0600

    :(

--- srcs/requirement/wordpress/conf/config.sh
+++ srcs/requirement/wordpress/conf/config.sh
@@ -4,7 +4,7 @@ sleep 1
 printf "Lunch config.sh\n"
 if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
 	printf "No wp-config.php found\n"
-	wp config create --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_USER --dbpass=$SQL_ROOT_PW --dbhost=mariadb:3306 --path='/var/www/wordpress'
+	wp config create --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_ROOT --dbpass=$SQL_USER_PW --dbhost=mariadb:3306 --path='/var/www/wordpress'
 	printf "mais connard\n"
 
 	wp core install	--path="/var/www/wordpress" --allow-root --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$SQL_ROOT --admin_password=$SQL_ROOT_PW --admin_email=$SQL_ROOT_MAIL
