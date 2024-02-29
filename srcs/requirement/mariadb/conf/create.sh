3bf2fd4562324f99ed6d5599d9992beb848c5266
llevasse <leonardlevasseur@yahoo.com>
2024-02-29 15:56:39 -0600

    :/

--- srcs/requirement/mariadb/conf/create.sh
+++ srcs/requirement/mariadb/conf/create.sh
@@ -1,2 +1,12 @@
 service mariadb start
-mariadb -u root -p${SQL_ROOT_PW} -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`; CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_USER_PW}'; GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_USER_PW}'; ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PW}'; FLUSH PRIVILEGES;"
+echo cc1
+mariadb -uroot -p${SQL_ROOT_PW} -e "CREATE DATABASE IF NOT EXISTS \`${SQL_DATABASE}\`;"
+echo cc2
+mariadb -uroot -p${SQL_ROOT_PW} -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${SQL_ROOT_PW}';"
+echo cc3
+mariadb -uroot -p${SQL_ROOT_PW} -e "CREATE USER IF NOT EXISTS \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_USER_PW}';"
+echo cc4
+mariadb -uroot -p${SQL_ROOT_PW} -e "GRANT ALL PRIVILEGES ON \`${SQL_DATABASE}\`.* TO \`${SQL_USER}\`@'%' IDENTIFIED BY '${SQL_USER_PW}';"
+echo cc5
+mariadb -uroot -p${SQL_ROOT_PW} -e "FLUSH PRIVILEGES;"
+echo cc6
--- srcs/requirement/wordpress/conf/config.sh
+++ srcs/requirement/wordpress/conf/config.sh
@@ -3,8 +3,15 @@ printf "Lunch config.sh (sleep 1)\n"
 sleep 1
 printf "Lunch config.sh\n"
 if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
+	wp core download --path=/var/www/wordpress --allow-root
 	printf "No wp-config.php found\n"
-	wp config create --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_ROOT --dbpass=$SQL_USER_PW --dbhost=mariadb:3306 --path='/var/www/wordpress'
+
+	cp /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php \
+		&& sed -i "s/database_name_here/${SQL_DATABASE}/" var/www/wordpress/wp-config.php \
+		&& sed -i "s/username_here/${SQL_USER}/" var/www/wordpress/wp-config.php \
+		&& sed -i "s/password_here/${SQL_USER_PW}/" var/www/wordpress/wp-config.php \
+		&& sed -i "s/localhost/mariadb:3306/" var/www/wordpress/wp-config.php
+#	wp config create --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_ROOT --dbpass=$SQL_USER_PW --dbhost=mariadb:3306 --path='/var/www/wordpress'
 	printf "mais connard\n"
 
 	wp core install	--path="/var/www/wordpress" --allow-root --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$SQL_ROOT --admin_password=$SQL_ROOT_PW --admin_email=$SQL_ROOT_MAIL
@@ -13,6 +20,7 @@ if [ ! -f "/var/www/wordpress/wp-config.php" ]; then
 fi
 
 mkdir -p /run/php
+while 1
 printf "php-fpm7.4\n"
 php-fpm7.4 -F
 
