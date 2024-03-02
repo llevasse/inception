c43d9f90bc5f257e7907095f018ea0ee810b195f
llevasse <leonardlevasseur@yahoo.com>
2024-03-02 19:34:33 +0100

    yeahhhhhhhhhhh :D

--- srcs/requirement/wordpress/conf/config.sh
+++ srcs/requirement/wordpress/conf/config.sh
@@ -3,15 +3,18 @@ printf "Lunch config.sh (sleep 1)\n"
 sleep 5
 printf "Lunch config.sh\n"
 	wp core download --path=/var/www/wordpress --allow-root
-	printf "No wp-config.php found\n"
 
-	cp /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php \
-		&& sed -i "s/database_name_here/${SQL_DATABASE}/" var/www/wordpress/wp-config.php \
-		&& sed -i "s/username_here/${SQL_USER}/" var/www/wordpress/wp-config.php \
-		&& sed -i "s/password_here/${SQL_USER_PW}/" var/www/wordpress/wp-config.php \
-		&& sed -i "s/localhost/mariadb:3306/" var/www/wordpress/wp-config.php
+	cp /var/www/wordpress/wp-config-sample.php /var/www/wordpress/wp-config.php
+	sed -i "s/database_name_here/${SQL_DATABASE}/" var/www/wordpress/wp-config.php
+	sed -i "s/username_here/${SQL_USER}/" var/www/wordpress/wp-config.php
+	sed -i "s/password_here/${SQL_USER_PW}/" var/www/wordpress/wp-config.php
+	sed -i "s/localhost/mariadb:3306/" var/www/wordpress/wp-config.php
+
+	sed -i "s/votre_nom_de_bdd/${SQL_DATABASE}/" var/www/wordpress/wp-config.php
+	sed -i "s/votre_utilisateur_de_bdd/${SQL_USER}/" var/www/wordpress/wp-config.php
+	sed -i "s/votre_mdp_de_bdd/${SQL_USER_PW}/" var/www/wordpress/wp-config.php
+	sed -i "s/localhost/mariadb:3306/" var/www/wordpress/wp-config.php
 #	wp config create --allow-root --dbname=$SQL_DATABASE --dbuser=$SQL_ROOT --dbpass=$SQL_USER_PW --dbhost=mariadb:3306 --path='/var/www/wordpress'
-	printf "mais connard\n"
 
 	wp core install	--path="/var/www/wordpress" --allow-root --url=$DOMAIN_NAME --title=$WP_TITLE --admin_user=$SQL_ROOT --admin_password=$SQL_ROOT_PW --admin_email=$SQL_ROOT_MAIL
 
